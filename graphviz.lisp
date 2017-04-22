c635ecab5bcdfe95eaba70a2e8a8ee73a433a983
Loy van Beek <loy.vanbeek@gmail.com>
2017-04-22 11:13:58 +0200

    Finish chapter 7 of Land of Lisp

--- graphviz.lisp
+++ graphviz.lisp
@@ -1,3 +1,5 @@
+(defparameter *max-label-length* 30)
+
 (defun dot-name (exp)
     (substitute-if #\_ (complement #'alphanumericp) (prin1-to-string  exp)))
 
@@ -49,3 +51,30 @@
     (dot->png fname 
         (lambda ()
             (graph->dot nodes edges))))
+
+(defun uedges->dot (edges)
+    (maplist 
+      (lambda (lst)
+        (mapc 
+          (lambda (edge)
+            (unless (assoc (car edge) (cdr lst))
+            (fresh-line)
+            (princ (dot-name (caar lst)))
+            (princ "--")
+            (princ (dot-name (car edge)))
+            (princ "[label=\"")
+            (princ (dot-label (cdr edge)))
+            (princ "\"];")))
+          (cdar lst)))
+      edges))
+
+(defun ugraph->dot (nodes edges)
+    (princ "graph{")
+    (nodes->dot nodes)
+    (uedges->dot edges)
+    (princ "}"))
+
+(defun ugraph->png (fname nodes edges)
+        (dot->png fname 
+                  (lambda () 
+                    (ugraph->dot nodes edges))))
